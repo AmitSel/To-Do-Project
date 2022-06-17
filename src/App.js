2bf6f215e197e87d7543d3e592dee9e7f4becb90
Amit Sela <lgg2amit95@cs.colman.ac.il>
2022-06-17 20:45:25 +0300

    Second Stage of Changes

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ import TodoList from './components/TodoList';
 
 function App() {
   const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>Amit's ToDo List</h1>
       </header>
       {/* HTML Element */}
-      <Form/>
+      <Form
+        todos={todos} 
+        setTodos={setTodos} 
+        inputText = {inputText} 
+        setInputText = {setInputText}
+      />
       <TodoList/>
     </div>
   );
